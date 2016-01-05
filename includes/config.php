b6d37c1c0b98a01b5622338a4c9be26f7003247c
Justin Deardorff <justindeardorff@gmail.com>
2016-01-04 20:21:50 -0500

    started adding PHP, removedCSS folder

---
+++ includes/config.php
@@ -0,0 +1,23 @@
+<?php
+ 
+    /**
+     * config.php
+     *
+     * CS75
+     * Project 2
+     *
+     * Configures pages.
+     */
+ 
+    // display errors, warnings, and notices
+    ini_set("display_errors", true);
+    error_reporting(E_ALL);
+ 
+    // requirements
+    //require("constants.php");
+    //require("functions.php");
+ 
+    // enable sessions
+    session_start();
+ 
+?>
